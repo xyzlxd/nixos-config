4330dac3350d955edd4eeb9bcffe4482bb7f9844
Asher <asher@lxd.xyz>
2025-07-15 12:52:33 +0000

    enable system environment packages: vim wget gitMinimal

--- machines/vm-shared.nix
+++ machines/vm-shared.nix
@@ -74,10 +74,11 @@
 
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    gitMinimal 
+  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
