f5fdd34d4d1e026e41f4df866a88e5c3968ef28e
Asher <asher@lxd.xyz>
2025-07-16 09:46:25 +0000

    git: Based on the configuration methods described in https://blog.gitbutler.com/how-git-core-devs-configure-git/

--- users/asher/git/default.nix
+++ users/asher/git/default.nix
@@ -3,5 +3,29 @@
     enable = true;
     userName = "Asher";
     userEmail = "asher@lxd.xyz";
+    # https://blog.gitbutler.com/how-git-core-devs-configure-git/
+    extraConfig = {
+      column.ui = "auto";
+      branch.sort = "-committerdate";
+      tag.sort = "version:refname";
+      init.defaultBranch = "main";
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
+      push = {
+        default = "simple"; # (default since 2.0)
+        autoSetupRemote = true;
+        followTags = true;
+      };
+      fetch = {
+        prune = true;
+        pruneTags = true;
+        all = true;
+      };
+
+    };
   };
 }
