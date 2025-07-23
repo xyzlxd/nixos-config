2d136c3f6704e7bc44858233b46edf1fef394693
Asher <asher@lxd.xyz>
2025-07-23 01:09:55 +0000

    nixvim: config more action like tab  use 4 space replace

--- users/asher/nixvim/options/default.nix
+++ users/asher/nixvim/options/default.nix
@@ -4,5 +4,9 @@
     relativenumber = true;
     # https://neovim.io/doc/user/usr_30.html#_changing-tabs-in-spaces-(and-back)
     expandtab = true;
+    autoindent = true;
+    smartindent = true;
+    tabstop = 4;
+    shiftwidth = 4;
   };
 }
